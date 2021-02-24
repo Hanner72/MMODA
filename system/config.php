5ef489d166841e93581a19337bc45c7247bd9002
Hanner72 <johann.danner@gmail.com>
2021-02-24 22:25:28 +0100

    Seitenlayout erstellen Beginn

--- system/config.php
+++ system/config.php
@@ -1,24 +1,33 @@
 <?php
 
-// DATENBANK
+// DATENBANK #######################################################
 $dbserver       = "localhost";
 $dbuname        = "root";
 $dbpass         = "";
 $db             = "mmoda";
 
-// DATENBANK VERBINDUNG
+
+// DATENBANK VERBINDUNG ############################################
 $conn = new mysqli($dbserver, $dbuname, $dbpass, $db);
 // CHECK VERBINDUNG
 if ($conn->connect_error)
 {
   die("Datenbankverbindung fehlgeschlagen: ".$conn->connect_error);
 }
-echo "Verbindung zu DB steht";
+else{
+  // Wenn die Verbindung steht, dann Daten von Datenbank holen
+  $kon_query = "SELECT titel, seperator, beschreibung, maintenance FROM konfiguration";
+  $kon_result = mysqli_query($conn, $kon_query);
+  $kon_row = mysqli_fetch_assoc($kon_result);
 
-// ADMINISTRATION
-$maintenance    = "0";
+  $titel = $kon_row['titel'];
+  $seperator = $kon_row['seperator'];
+  $beschreibung = $kon_row['beschreibung'];
+  $maintenance = $kon_row['maintenance'];
+}
 
-// SEITENEINSTELLUNGEN
+
+// SEITENEINSTELLUNGEN #############################################
 $titel          = "MMODA";
 $seperator      = " - ";
 $beschreibung   = "Das neue MMO Spiel";
---
+++ system/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function getPage()
+{
+  if(isset($_GET['page']))
+  {
+    $page = $_GET['page'];
+    if($page == "index")
+    {
+      require_once "frontend/pages/index.php";
+    }
+    elseif($page == "contact")
+    {
+      require_once "frontend/pages/contact.php";
+    }
+  }
+  else
+  {
+    require_once "frontend/pages/index.php";
+  }
+}
+
+?>
